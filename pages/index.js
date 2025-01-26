4e587587f7692118143f795781a99736def1fd40
Doomcaster1917 <Webtalestoday@gmail.com>
2025-01-26 14:08:41 +0300

    InitData test

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,13 @@ import { retrieveLaunchParams } from '@telegram-apps/sdk';
 
 
 const Index = () => {
+
+    const { initDataRaw, initData} = retrieveLaunchParams()
     return (
         <div>
-            <iframe allow="clipboard-write" style={{width: 450+"px", height: 50+"px"}} src={"https://music.yandex.ru/iframe/track/78022555"}>
-            </iframe>
+            {/*<iframe allow="clipboard-write" style={{width: 450+"px", height: 50+"px"}} src={"https://music.yandex.ru/iframe/track/78022555"}>*/}
+            {/*</iframe>*/}
+            {initData}
         </div>
     );
 };
