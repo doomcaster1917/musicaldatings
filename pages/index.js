768433da919b4edf391a0f79b3780b8d78207d41
Doomcaster1917 <Webtalestoday@gmail.com>
2025-01-26 12:42:12 +0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { retrieveLaunchParams } from '@telegram-apps/sdk';
+
+
+const Index = () => {
+    return (
+        <div>
+            <iframe allow="clipboard-write" style={{width: 450+"px", height: 50+"px"}} src={"https://music.yandex.ru/iframe/track/78022555"}>
+            </iframe>
+        </div>
+    );
+};
+
+export default Index;
